8a93a406672dd4a8681fe75e7707f262698cd2e6
Damon Pham <damondpham@gmail.com>
2022-11-04 04:55:08 +0700

    idk why this was undone

--- tests/run_fMRItools_tests.R
+++ tests/run_fMRItools_tests.R
@@ -1,14 +1,8 @@
 # Build --> Install and Restart
-# [Edit this] path to the Workbench for your computer.
-my_wb <- "~/Desktop/workbench"
 
 library(testthat)
 library(fMRItools)
 
-if (interactive()) {
-  library(ciftiTools)
-  ciftiTools.setOption("wb_path", my_wb)
-}
 tests_dir <- "testthat"
 if (!endsWith(getwd(), "tests")) { tests_dir <- file.path("tests", tests_dir) }
 
