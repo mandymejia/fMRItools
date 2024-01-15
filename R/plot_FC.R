b420880ccf396b6ed08eb9fb597a42a5fceb3354
Damon Pham <damondpham@gmail.com>
2024-01-15 21:07:52 +0700

    update colors again

--- R/plot_FC.R
+++ R/plot_FC.R
@@ -40,10 +40,15 @@ color_palette <- function(pal="Beach") {
   switch(pal,
     Beach = ciftiTools::expand_color_pal(data.frame(
       color = c(
-        "#1e208a", "#3d40eb", "#789ae0", "#90d5f5",
-        "#def7f7", "#fafffa", "#f9f9da",
-        "#f2d089", "#d18b69", "#b32f2b", "#6b0e0e"
+        "#181c7a", "#3235ed", "#4a7ef7", "#96d5fa",
+        "#e1f7f8", "#fbfff9", "#f9f9d8",
+        "#f5d584", "#cf733e", "#ac2d1f", "#670c04"
       ),
+      # color = c(
+      #   "#1e208a", "#3d40eb", "#789ae0", "#90d5f5",
+      #   "#def7f7", "#fafffa", "#f9f9da",
+      #   "#f2d089", "#d18b69", "#b32f2b", "#6b0e0e"
+      # ),
       # color = c(
       #   "#23237d", "#404eb8", "#87b6e6", "#b6e6f0",
       #   "#ddf6f6", "#f7fff7", "#f2f2d8",
