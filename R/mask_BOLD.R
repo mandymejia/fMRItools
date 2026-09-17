93d43f9895b69ce6f95449a3d6333002e0a9010e
Damon Pham <damondpham@gmail.com>
2026-09-17 18:11:52 +0700

    Move `mask_BOLD` from BayesBrainMap to here

---
+++ R/mask_BOLD.R
@@ -0,0 +1,47 @@
+#' Create a mask based on vertices that are invalid
+#'
+#' @param BOLD A \eqn{V \times T} numeric matrix. Each row is a location.
+#' @param meanTol,varTol Tolerance for mean and variance of each data location.
+#'  Locations which do not meet these thresholds are masked out of the analysis.
+#'  Defaults: \code{-Inf} for \code{meanTol} (ignore), and \code{1e-6} for
+#'  \code{varTol}.
+#' @param verbose Print messages counting how many locations are removed?
+#'
+#' @importFrom matrixStats rowVars
+#' @return A logical vector indicating valid vertices
+#'
+#' @keywords internal
+mask_BOLD <- function(BOLD, meanTol=-Inf, varTol=1e-6, verbose=TRUE){
+  stopifnot(is.matrix(BOLD))
+
+  mask_na <- mask_mean <- mask_var <- rep(TRUE, nrow(BOLD))
+  # Mark columns with any NA or NaN values for removal.
+  mask_na[apply(is.na(BOLD) | is.nan(BOLD), 1, any)] <- FALSE
+  # Calculate means and variances of columns, except those with any NA or NaN.
+  # Mark columns with mean/var falling under the thresholds for removal.
+  mask_mean[mask_na][rowMeans(BOLD[mask_na,,drop=FALSE]) < meanTol] <- FALSE
+  if (varTol > 0) {
+    mask_var[mask_na][matrixStats::rowVars(BOLD[mask_na,,drop=FALSE]) < varTol] <- FALSE
+  }
+
+  # Print counts of locations removed, for each reason.
+  if (verbose) {
+    warn_part1 <- if (any(!mask_na)) { "additional locations" } else { "locations" }
+    if (any(!mask_na)) {
+      cat(sum(!mask_na), paste0("locations removed due to NA/NaN values.  "))
+    }
+    # Do not include NA locations in count.
+    mask_mean2 <- mask_mean | (!mask_na)
+    if (any(!mask_mean2)) {
+      cat(sum(!mask_mean2), warn_part1, paste0("removed due to low mean.  "))
+    }
+    # Do not include NA or low-mean locations in count.
+    mask_var2 <- mask_var | (!mask_mean) | (!mask_na)
+    if (any(!mask_var2)) {
+      cat(sum(!mask_var2), warn_part1, paste0("removed due to low variance.  "))
+    }
+  }
+
+  # Return composite mask.
+  mask_na & mask_mean & mask_var
+}
